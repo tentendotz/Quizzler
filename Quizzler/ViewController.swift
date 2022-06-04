cfd2b4680747cb0e0d855a5ce9038a609e36a148
ttt-net <tenten102matsuyama@gmail.com>
2022-06-05 02:41:51 +0900

    Add IBOutlet and IBAction

--- Quizzler/ViewController.swift
+++ Quizzler/ViewController.swift
@@ -8,10 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
     }
 
 
