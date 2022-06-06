6ed225f46fb8f46e39ecb648fdd869a4379c9c65
ttt-net <tenten102matsuyama@gmail.com>
2022-06-07 03:22:39 +0900

    Add quiz logic

--- Quizzler/ViewController.swift
+++ Quizzler/ViewController.swift
@@ -14,15 +14,44 @@ class ViewController: UIViewController {
     @IBOutlet weak var progressBar: UIProgressView!
     @IBOutlet weak var trueButton: UIButton!
     @IBOutlet weak var falseButton: UIButton!
+    
+    var questionNumber = 0
+    var score = 0
+    var actualAnswer = ""
+    
+     let quiz = [
+     ["Four + Two is equal to Six.", "True"],
+     ["Five - Three is greater than One.", "True"],
+     ["Three + Eight is less than Ten.", "False"]
+     ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        questionLabel.text = quiz[questionNumber][0]
     }
     
     @IBAction func answerButtonPressed(_ sender: UIButton) {
+        let userAnswer = sender.currentTitle!
+        actualAnswer = quiz[questionNumber][1]
+        checkAnswer(userAnswer)
+        questionLabel.text = quiz[questionNumber][0]
+    }
+    
+    func checkAnswer(_ userAnswer: String)  {
+        if userAnswer == actualAnswer {
+            score += 1
+        }
+        nextQuestion()
     }
 
-
+    func nextQuestion() {
+        if questionNumber + 1 < quiz.count {
+            questionNumber += 1
+        } else {
+            questionNumber = 0
+            score = 0
+        }
+    }
+    
 }
 
